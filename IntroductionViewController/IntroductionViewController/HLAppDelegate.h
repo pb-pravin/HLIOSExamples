610c269b4b20290feb1d6ebb88eba397dcd68e56
Hailin <anhailin@mozat.com>
2013-01-07 22:30:35 +0800

    Add a new project IntroductionDemo

---
+++ IntroductionViewController/IntroductionViewController/HLAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HLAppDelegate.h
+//  IntroductionViewController
+//
+//  Created by an hailin on 1/5/13.
+//  Copyright (c) 2013 HailinAn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIViewController *viewController;
+
+
+@end
---
+++ IntroductionViewController/IntroductionViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IntroductionViewController
+//
+//  Created by an hailin on 1/5/13.
+//  Copyright (c) 2013 HailinAn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HLAppDelegate class]));
+    }
+}
